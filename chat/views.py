57ab8cfb338735a571d0d85dc9a547512897c55b
mayn-k <mayank007mk7@gmail.com>
2023-06-27 09:51:13 +0530

    Views & URLs

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
+from chat.models import Room
+
 # Create your views here.
+# /chat/ - chat room selector
+def index_view(request):
+    return render(request, 
+                  'index.html', 
+                  {'rooms': Room.objects.all(), }
+                  )
+
+
+# /chat/<ROOM_NAME>/ - chat room
+def room_view(request, room_name):
+    chat_room, created = Room.objects.get_or_create(name = room_name)
+    return render(request,
+                  'room.html',
+                  {'room':chat_room,}
+                  )
